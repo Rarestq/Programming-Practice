226287d8195b16354a1052aa3973d7bbee6e3be2
Rarestq <rarestzhou@foxmail.com>
2022-09-28 16:04:01 +0800

    public static int countBits(int n) {

--- codewars/src/main/java/com/rarestzhou/codewars/september/BitCounting.java
+++ codewars/src/main/java/com/rarestzhou/codewars/september/BitCounting.java
@@ -6,11 +6,31 @@ package com.rarestzhou.codewars.september;
  * @author: wuxiu
  * @date: 2022/9/28 2:59 PM
  * @description: https://www.codewars.com/kata/526571aae218b8ee490006f4/train/java
+ * <p>
+ * Write a function that takes an integer as input, and returns the number of bits that are equal to
+ * one in the binary representation of that number. You can guarantee that input is non-negative.
+ * <p>
+ * Example: The binary representation of 1234 is 10011010010, so the function should return 5
+ * in this case
  */
 public class BitCounting {
 
-    public static int countBits(int n){
+    public static int countBits(int n) {
         // Show me the code!
-        return 0;
+        if (n == 0) {
+            return 0;
+        }
+        return (int) Integer.toBinaryString(n).chars()
+                .filter(c -> c == '1')
+                .count();
+    }
+
+    public static int countBitsEasy(int n) {
+        return Integer.bitCount(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countBits(1234));
+        System.out.println(countBitsEasy(1234));
     }
 }
